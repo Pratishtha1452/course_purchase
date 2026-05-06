8cd74a60c7d6c28003638d67fca938832a0d3be2
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-05-07 02:26:56 +0530

    arr test

--- src/test/java/Testcourse_purchase.java
+++ src/test/java/Testcourse_purchase.java
@@ -2,14 +2,23 @@ import course_purchase.Course_Purchased;
 import course_purchase.JavaC;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Testcourse_purchase {
+
+    int [] arr1 = {1, 2, 3};
+    int [] arr2 = arr1;
 
     @Test
     void testcoursePurchased(){
         Course_Purchased obj = new Course_Purchased();
         boolean purchase = obj.coursePurchased(new JavaC());
         assertTrue(purchase);
+    }
+
+    @Test
+    void arrtest(){
+        assertEquals(arr1,arr2);
     }
 }
