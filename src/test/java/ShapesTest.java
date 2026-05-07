0b6d8192c5a63897913f3d7359dfbcc3b126cce6
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-05-07 13:00:03 +0530

    other annotations

--- src/test/java/ShapesTest.java
+++ src/test/java/ShapesTest.java
@@ -1,18 +1,27 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import otherTests.Shapes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShapesTest {
     Shapes obj = new Shapes();
+    Shapes shape;
+
+    @BeforeAll
+    static void init(){
+
+        System.out.println("Before test");
+    }
 
     @Test
     void testsqarea(){
-        assertEquals(78.5, obj.sqarea(12));
+        assertEquals(144, obj.sqarea(12));
+        System.out.println(144);
     }
 
     @Test
     void testcirclearea(){
         assertEquals(78.5, obj.circlearea(5));
+        System.out.println(78.5);
     }
 }
